43355a90aa2e41cf7c066146395e1158475d9070
codesg04 <95615228+codesg04@users.noreply.github.com>
2022-01-24 18:59:26 +0530

    t3 dne

---
+++ task3/data.js
@@ -0,0 +1,66 @@
+var ins = document.getElementById("insert");
+var id_arr = [0,1];
+
+for(var i=0;i<arr.length;i++){
+    var show="<div Id="+i+" class='note'><h1>"+arr[i].title+"</h1><p>"+arr[i].content+"</p><button name="+i+" onclick='removeFun(name)' class='delete' >X</button></div>";
+    //console.log(show);
+    ins.insertAdjacentHTML("afterend",show);
+}
+
+var btn = document.getElementById("create-note");
+btn.onclick = function(){
+    var title=document.getElementById("title-input").value;
+    var note=document.getElementById("note-input").value;
+    document.getElementById("title-input").value="";
+    document.getElementById("note-input").value="";
+    
+    var idd=-1;
+    if(id_arr.length==0){
+        idd=0;
+        id_arr.push(0);
+    }else{
+        for(var i=0;i<id_arr.length;i++){
+            if(id_arr[i]===-1) { id_arr[i]=i; idd=i; break;}
+        }
+        if(idd===-1) {idd=id_arr.length; id_arr.push(idd);}
+    }
+
+    if(title.length!=0 && note.length!=0){
+        var show="<div id="+idd+" class='note'><h1>"+title+"</h1><p>"+note+"</p><button name="+idd+" onclick='removeFun(name)' class='delete' >X</button></div>";
+        ins.insertAdjacentHTML("afterend",show); 
+    } 
+    else{
+        alert(" Please Enter title and notes");
+    }
+    //console.log(show);
+
+}
+
+function removeFun(param){
+    //console.log(param);
+
+    var myobf=document.getElementById(param);
+    console.log(myobf);
+    if(myobf!=null){
+        myobf.remove();
+        for(var i=0;i<id_arr.length;i++){
+            if(id_arr[i]==param){ id_arr[i]=-1; break;}
+        }
+        // for(var i=0;i<id_arr.length;i++){
+        //     console.log(id_arr[i]);
+        // }
+    }
+}
+
+
+
+
+// const numArr = [23, 45, 67, 89];
+
+// // remove element 45
+// // at index 1 and
+// // pass 1 as second argument
+// // to tell method to
+// // delete 1 element
+// from the starting index
+// numArr.splice(1, 1);
